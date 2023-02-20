5c2c176f7d6bfdfb01fb9836ca166ba9d8cf4502
Spencer A Warren <spencer.warren.19@cnu.edu>
2023-02-20 15:10:44 -0500

    Event Object Tests

---
+++ JavaCon/src/test/java/com/javacon/EventTest.java
@@ -0,0 +1,30 @@
+package com.javacon;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventTest {
+
+    @org.junit.jupiter.api.Test
+    void getNameTest() {
+        Event event = new Event("Event 1", 60);
+        assertEquals("Event 1", event.getName());
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDurationTest() {
+        Event event = new Event("Event 1", 60);
+        assertEquals(60, event.getDuration());
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToStringTest1() {
+        Event event = new Event("Event 1", 60);
+        assertEquals("Event 1 60min", event.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToStringTest2() {
+        Event event = new Event("Event 1", 5);
+        assertEquals("Event 1 lightning", event.toString());
+    }
+}

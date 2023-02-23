63a4360364f9dec8835a062ed12d9d5b548e1cf1
Spencer A Warren <spencer.warren.19@cnu.edu>
2023-02-23 11:19:21 -0500

    SonarLint Issues

--- JavaCon/src/main/java/com/javacon/ParcingCon.java
+++ JavaCon/src/main/java/com/javacon/ParcingCon.java
@@ -2,19 +2,21 @@ package com.javacon;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ParcingCon {
+    private ParcingCon() {
+        // Private Constructor
+    }
 
-
-    public static ArrayList<SimpleEvent> getEvents(){
+    public static List<SimpleEvent> getEvents(){
         return getEvents("resources/input.txt");
     }
 
     //for testing purposes
-    public static ArrayList<SimpleEvent> getEvents(String path) {
-        try {
-            Scanner scan = new Scanner(Path.of(path));
+    public static List<SimpleEvent> getEvents(String path) {
+        try (Scanner scan = new Scanner(Path.of(path))){
             ArrayList<SimpleEvent> simpleEvents = new ArrayList<>();
             while (scan.hasNext()) {
                 String row;
@@ -24,8 +26,6 @@ public class ParcingCon {
                 if (row.contains("lightning")) {
                     for (String splits : row.split("lightning")) {
                         splits = splits.trim();
-//                        System.out.println("Lightning Split Test:");
-//                        System.out.println(splits);
                         if (!hasSplit) {
                             //Adds the Name of the Event if it is a Lightning Topic
                             //then skips the remaining half as it isn't needed.
@@ -40,8 +40,6 @@ public class ParcingCon {
                     if (row.contains("30min")) {
                         for (String splits : row.split("30")) {
                             splits = splits.trim();
-//                    System.out.println("30min Split Test:");
-//                    System.out.println(splits);
                             if (!hasSplit) {
                                 //Adds the Name of the Event if it is a 30min Topic
                                 //then skips the remaining half as it isn't needed.
@@ -54,8 +52,6 @@ public class ParcingCon {
 
                     else if (row.contains("45min")) {
                         for (String splits : row.split("45min")) {
-//                            System.out.println("45min Split Test:");
-//                            System.out.println(splits);
                             splits = splits.trim();
                             if (!hasSplit) {
                                 //Adds the Name of the Event if it is a 45min Topic
@@ -70,8 +66,6 @@ public class ParcingCon {
                     else if (row.contains("60min")) {
                         for (String splits : row.split("60min")) {
                             splits = splits.trim();
-//                            System.out.println("60min Split Test:");
-//                            System.out.println(splits);
                             if (!hasSplit) {
                                 //Adds the Name of the Event if it is a 60min Topic
                                 //then skips the remaining half as it isn't needed.
@@ -85,8 +79,6 @@ public class ParcingCon {
                     else if (row.contains("15min")) {
                         for (String splits : row.split("15")) {
                             splits = splits.trim();
-//                            System.out.println("15min Split Test:");
-//                            System.out.println(splits);
                             if (!hasSplit) {
                                 //Adds the Name of the Event if it is a 15min Topic
                                 //then skips the remaining half as it isn't needed.
@@ -100,8 +92,8 @@ public class ParcingCon {
             }
             return simpleEvents;
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
-        return null;
+        return new ArrayList<>();
     }
 }
